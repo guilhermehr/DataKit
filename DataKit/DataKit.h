d5226ea562b84273b2b3c2073e604bccaebf3728
Guilherme <guilherme@MacBook-Pro-de-Aloc-35.local>
2017-12-01 18:54:33 -0200

    Initial Commit

---
+++ DataKit/DataKit.h
@@ -0,0 +1,19 @@
+//
+//  DataKit.h
+//  DataKit
+//
+//  Created by Guilherme on 01/12/2017.
+//  Copyright © 2017 Guilherme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataKit.
+FOUNDATION_EXPORT double DataKitVersionNumber;
+
+//! Project version string for DataKit.
+FOUNDATION_EXPORT const unsigned char DataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataKit/PublicHeader.h>
+
+
